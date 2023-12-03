faf7b1f15509a79aa6907ad872e46196a9237801
KucharskiPiotr <29356216+KucharskiPiotr@users.noreply.github.com>
2023-12-03 15:31:43 +0100

    Day 1: Show results of day 1

--- src/main.rb
+++ src/main.rb
@@ -1,5 +1,10 @@
 require_relative 'ex1/facade'
 
-part_one_command = Ex1::PartOneCommand.new(Infra::InputReader.read('input1.txt'))
-result = Ex1::Facade.new.handle(part_one_command)
-puts result
+ex1_input = Infra::InputReader.read('input1.txt')
+ex1_facade = Ex1::Facade.new
+
+part_one_command = Ex1::PartOneCommand.new(ex1_input)
+puts ex1_facade.handle(part_one_command)
+
+part_two_command = Ex1::PartTwoCommand.new(ex1_input)
+puts ex1_facade.handle(part_two_command)
